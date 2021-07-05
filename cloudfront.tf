669eb124309eb817fe01ecd9a081f5d6d0c3605d
Alan Nix <alanneilnix@gmail.com>
2021-07-05 11:57:41 -0400

    fix: updating to latest TLS protocols

--- cloudfront.tf
+++ cloudfront.tf
@@ -51,7 +51,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
 
   viewer_certificate {
     acm_certificate_arn      = aws_acm_certificate_validation.default.certificate_arn
-    minimum_protocol_version = "TLSv1.2_2019"
+    minimum_protocol_version = "TLSv1.2_2021"
     ssl_support_method       = "sni-only"
   }
 
