7a60398818dab861c3ce58e99071225f6968d640
Alan Nix <alanneilnix@gmail.com>
2021-07-18 16:56:35 -0400

    fix: added public access block for S3

--- s3.tf
+++ s3.tf
@@ -30,6 +30,14 @@ resource "aws_s3_bucket" "main" {
   )
 }
 
+resource "aws_s3_bucket_public_access_block" "main" {
+  bucket                  = aws_s3_bucket.main.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
 data "aws_iam_policy_document" "bucket_policy" {
 
   statement {
